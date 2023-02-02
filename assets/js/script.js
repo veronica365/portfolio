2c8fbfabbaa828692b072404b4b409a6c7d2fa6d
veronica365 <naruth10@gmail.com>
2023-02-02 14:53:11 +0300

    Add the correct linters for the javascript challenge

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,40 @@
 // Functions to read elements by class or name
 const $ = (elementOrClass) => document.querySelector(elementOrClass);
-const menu = $(".mobile-nav");
-const wrap = $(".wrap");
-const menuButton = $(".menu-button");
+const menu = $('.mobile-nav');
+const wrap = $('.wrap');
+const menuButton = $('.menu-button');
 
 // Used to show or hide navigation modal
 function toggleNavigationModal() {
-  $(".toolbar").classList.toggle("open");
-  wrap.classList.toggle("mobile-menu-open");
+  $('.toolbar').classList.toggle('open');
+  wrap.classList.toggle('mobile-menu-open');
 }
 
 // Used to hide navigation modal on clicking x or menu links
 function hideNavigationModal() {
-  menu.classList.toggle("open");
+  menu.classList.toggle('open');
   toggleNavigationModal();
-  wrap.removeEventListener("click", () => null);
+  wrap.removeEventListener('click', () => null);
 }
 
 // show or hide the navigation menu on clicking .mobile-nav
-menu.addEventListener("click", function () {
-  this.classList.toggle("open");
+menu.addEventListener('click', () => {
+  menu.classList.toggle('open');
   toggleNavigationModal();
 });
 
 // show or hide the navigation menu on clicking .mobile-nav
-menuButton.addEventListener("click", function () {
+menuButton.addEventListener('click', () => {
   hideNavigationModal();
 });
 
-// Get all links, add click eventlisteners, click on one to scroll correct part of the page into view
-const allLinks = document.querySelectorAll(".menu-item");
-allLinks.forEach(function (link) {
-  link.addEventListener("click", function (event) {
+// Get all links, add click eventlisteners, click on one to
+// scroll correct part of the page into view
+const allLinks = document.querySelectorAll('.menu-item');
+allLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
     event.preventDefault();
     hideNavigationModal();
-    $(link.hash).scrollIntoView({ behavior: "smooth" });
+    $(link.hash).scrollIntoView({ behavior: 'smooth' });
   });
 });
