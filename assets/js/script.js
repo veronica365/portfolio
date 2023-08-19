22697af0786c6f88c4c5a0e4794ebcddeac68d9c
veronica365 <naruth10@gmail.com>
2023-08-19 08:17:28 +0300

    Add clean up functionality to introduce a toast

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,56 +7,56 @@ const $ = (elementOrClass) => document.querySelector(elementOrClass);
 const allProjectsArray = [
   {
     id: 1,
-    title: 'Tonic',
-    image: './assets/img/portfolio-1.svg',
-    altText: 'project image',
-    company: 'CANOPY',
-    role: 'Backend Dev',
+    title: "Tonic",
+    image: "./assets/img/portfolio-1.svg",
+    altText: "project image",
+    company: "CANOPY",
+    role: "Backend Dev",
     year: 2015,
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required',
-    skills: ['html', 'css', 'javascript'],
+      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+    skills: ["html", "css", "javascript"],
   },
   {
     id: 2,
-    title: 'Multi-Post Stories',
-    image: './assets/img/portfolio-2.svg',
-    altText: 'project image',
-    company: 'CANOPY',
-    role: 'Backend Dev',
+    title: "Multi-Post Stories",
+    image: "./assets/img/portfolio-2.svg",
+    altText: "project image",
+    company: "CANOPY",
+    role: "Backend Dev",
     year: 2015,
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required',
-    skills: ['html', 'css', 'javascript'],
+      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+    skills: ["html", "css", "javascript"],
   },
   {
     id: 3,
-    title: 'Tonic',
-    image: './assets/img/portfolio-3.svg',
-    altText: 'project image',
-    company: 'CANOPY',
-    role: 'Backend Dev',
+    title: "Tonic",
+    image: "./assets/img/portfolio-3.svg",
+    altText: "project image",
+    company: "CANOPY",
+    role: "Backend Dev",
     year: 2015,
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required',
-    skills: ['html', 'css', 'javascript'],
+      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+    skills: ["html", "css", "javascript"],
   },
   {
     id: 4,
-    title: 'Multi-Post Stories',
-    image: './assets/img/portfolio-4.svg',
-    altText: 'project image',
-    company: 'CANOPY',
-    role: 'Backend Dev',
+    title: "Multi-Post Stories",
+    image: "./assets/img/portfolio-4.svg",
+    altText: "project image",
+    company: "CANOPY",
+    role: "Backend Dev",
     year: 2015,
     description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required',
-    skills: ['html', 'css', 'javascript'],
+      "A daily selection of privately personalized reads; no accounts or sign-ups required",
+    skills: ["html", "css", "javascript"],
   },
 ];
 
 const generateWorkSkils = (skills, element) => {
-  let innerHTML = '';
+  let innerHTML = "";
   skills.forEach((skill) => {
     innerHTML += `<${element} class="skill">${skill}</${element}>`;
   });
@@ -64,7 +64,7 @@ const generateWorkSkils = (skills, element) => {
 };
 
 const generateWorkProjects = (works) => {
-  let innerHTML = '';
+  let innerHTML = "";
   works.forEach((project) => {
     innerHTML += `<article class="media">
     <div class="media-img img-1">
@@ -83,12 +83,12 @@ const generateWorkProjects = (works) => {
         <p>${project.description}</p>
       </div>
       <ul class="skills">
-      ${generateWorkSkils(project.skills, 'li')}
+      ${generateWorkSkils(project.skills, "li")}
       </ul>
       <div class="read-more">
         <a href="#" class="read-more-link" data-project-id="${
-  project.id
-}"> See Project </a>
+          project.id
+        }"> See Project </a>
       </div>
     </div>
   </article>`;
@@ -96,7 +96,7 @@ const generateWorkProjects = (works) => {
   return innerHTML;
 };
 
-const works = $('#works').getElementsByClassName('content')?.[0];
+const works = $("#works").getElementsByClassName("content")?.[0];
 works.innerHTML = `${generateWorkProjects(allProjectsArray)}`;
 
 /**
@@ -126,7 +126,7 @@ const generateModal = (work) => `
         <p class="description">${work.description}</p>
         <div>
           <div class="skills">
-            ${generateWorkSkils(work.skills, 'span')}
+            ${generateWorkSkils(work.skills, "span")}
           </div>
           <div class="read-more">
             <a href="#">
@@ -141,19 +141,46 @@ const generateModal = (work) => `
     </div>
   `;
 
-const readMoreLinks = document.querySelectorAll('.read-more-link');
+/**
+ * Generate a toast html body
+ */
+const generateToast = (detail) => `
+  <div>
+    <h5>Hi ${detail.name}</h5>
+    <p>${detail.message}</p>
+  </div>`;
+
+const displayToast = ({ name, status, message }) => {
+  const toast = document.createElement("div");
+  toast.className = `message-toast ${status}`;
+  toast.innerHTML = generateToast({ name, message });
+  $(".wrap").appendChild(toast);
+  formButton.removeAttribute("disabled");
+  setTimeout(() => {
+    $(".message-toast").remove();
+  }, 5000);
+};
+
+const setInnerText = (element, text) => {
+  if (element.innerText) {
+    return (element.innerText = String(text));
+  }
+  element.textContent = String(text);
+};
+
+const readMoreLinks = document.querySelectorAll(".read-more-link");
 readMoreLinks.forEach((link) => {
-  link.addEventListener('click', (event) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault();
-    const workId = event.target.getAttribute('data-project-id');
+    const workId = event.target.getAttribute("data-project-id");
     const project = allProjectsArray.find(({ id }) => String(id) === workId);
-    const modal = document.createElement('div');
-    modal.id = 'modal';
-    modal.className = 'modal';
+    const modal = document.createElement("div");
+    modal.id = "modal";
+    modal.className = "modal";
     modal.innerHTML = generateModal(project);
     document.body.appendChild(modal);
-    $('.article-header .close-button')?.addEventListener('click', () => {
-      $('#modal').remove();
+    $(".article-header .close-button")?.addEventListener("click", () => {
+      $("#modal").remove();
     });
   });
 });
@@ -162,91 +189,97 @@ readMoreLinks.forEach((link) => {
  * Below are scripts to show the mobile menu onclicking the hamburger button
  */
 
-const menu = $('.mobile-nav');
-const wrap = $('.wrap');
-const menuButton = $('.menu-button');
+const menu = $(".mobile-nav");
+const wrap = $(".wrap");
+const menuButton = $(".menu-button");
+const formButton = $("#contact-me form button");
 
 // Used to show or hide navigation modal
 function toggleNavigationModal() {
-  $('.toolbar').classList.toggle('open');
-  wrap.classList.toggle('mobile-menu-open');
+  $(".toolbar").classList.toggle("open");
+  wrap.classList.toggle("mobile-menu-open");
 }
 
 // Used to hide navigation modal on clicking x or menu links
 function hideNavigationModal() {
-  menu.classList.toggle('open');
+  menu.classList.toggle("open");
   toggleNavigationModal();
-  wrap.removeEventListener('click', () => null);
+  wrap.removeEventListener("click", () => null);
 }
 
 // show or hide the navigation menu on clicking .mobile-nav
-menu.addEventListener('click', () => {
-  menu.classList.toggle('open');
+menu.addEventListener("click", () => {
+  menu.classList.toggle("open");
   toggleNavigationModal();
 });
 
 // show or hide the navigation menu on clicking .mobile-nav
-menuButton.addEventListener('click', () => {
+menuButton.addEventListener("click", () => {
   hideNavigationModal();
 });
 
 // Get all links, add click eventlisteners, click on one to
 // scroll correct part of the page into view
-const allLinks = document.querySelectorAll('.menu-item');
+const allLinks = document.querySelectorAll(".menu-item");
 allLinks.forEach((link) => {
-  link.addEventListener('click', (event) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault();
     hideNavigationModal();
-    $(link.hash).scrollIntoView({ behavior: 'smooth' });
+    $(link.hash).scrollIntoView({ behavior: "smooth" });
   });
 });
 
 /**
- * Below are scripts to save and populate the form data
+ *
+ * @param {*} url API that we are submitting to my user details
+ * @param {*} data Data that is being submitted to the API
+ * @returns {Object} Native javascript objects
  */
-const getSavedFormData = () => {
-  const data = localStorage.getItem('formData');
-  if (data) return JSON.parse(data);
-  return {};
-};
-
-function saveFormData(newFormData) {
-  let data = getSavedFormData();
-  data = JSON.stringify({ ...data, ...newFormData });
-  localStorage.setItem('formData', data);
+async function postData(data) {
+  const formData = new FormData();
+  formData.append("name", data.name);
+  formData.append("email", data.email);
+  formData.append("message", data.message);
+  try {
+    await fetch("https://formspree.io/f/moqzvdbe", {
+      method: "POST",
+      mode: "no-cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      body: formData,
+    });
+    displayToast({
+      ...data,
+      status: "success",
+      message: "Message submitted successfully.",
+    });
+  } catch (error) {
+    displayToast({
+      ...data,
+      status: "error",
+      message: "Submitting your message failed",
+    });
+  }
 }
-
-const formData = getSavedFormData();
-const fullName = $('#name');
-const email = $('#email');
-const message = $('#message');
-
-fullName.value = formData.name || '';
-email.value = formData.email || '';
-message.value = formData.message || '';
-
-[fullName, email, message].forEach((input) => {
-  input.addEventListener('change', () => {
-    saveFormData({ [input.name]: input.value });
-  });
-});
 
 /**
  * Below are scripts to validate the form before submitting
  */
-const contactForm = $('#contact-me form');
-const formMessage = $('#form-message');
+const contactForm = $("#contact-me form");
+const formMessage = $("#form-message");
 const setFormMessage = (message) => {
   formMessage.innerText = message;
-  if (!formMessage.matches('.form-message')) {
-    formMessage.classList.add('form-message');
+  if (!formMessage.matches(".form-message")) {
+    formMessage.classList.add("form-message");
   }
 };
 
-$('#contact-me form button').removeAttribute('disabled');
-contactForm.addEventListener('submit', (event) => {
+contactForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  let error = '';
+  let error = "";
   setFormMessage(error);
   const name = contactForm.elements.name.value.trim();
   const email = contactForm.elements.email.value.trim();
@@ -254,27 +287,32 @@ contactForm.addEventListener('submit', (event) => {
   let isValidEmail = false;
 
   if (!email || !message || !name) {
-    error = 'Error: Ensure that there are no blank fields';
+    error = "Error: Ensure that there are no blank fields";
+    displayToast({ name: name || "There", status: "error", message: error });
     return setFormMessage(error);
   }
 
-  isValidEmail = /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/gi.test(
-    email,
-  );
+  isValidEmail =
+    /^[a-z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/gi.test(
+      email
+    );
   if (!isValidEmail) {
-    error = 'Error: Ensure that you have typed the correct email';
+    error = "Error: Ensure that you have typed the correct email";
+    displayToast({ name: name || "There", status: "error", message: error });
     return setFormMessage(error);
   }
 
   if (email !== String(email).toLowerCase()) {
-    error = 'Error: Ensure that the email is typed in lowercase';
+    error = "Error: Ensure that the email is typed in lowercase";
+    displayToast({ name: name || "There", status: "error", message: error });
     return setFormMessage(error);
   }
-  saveFormData({ fullName: name, email, message });
   // This line is used to prevent multiple submitts as the previous submit
   // is still being processed
-  $('#contact-me form button').setAttribute('disabled', '');
-  contactForm.submit();
-  formMessage.classList.remove('form-message');
+  formButton.setAttribute("disabled", "");
+  setInnerText(formButton,"... submitting");
+  formMessage.classList.remove("form-message");
+  await postData({ name, email, message });
+  setInnerText(formButton, "Get in touch");
   return true;
 });
